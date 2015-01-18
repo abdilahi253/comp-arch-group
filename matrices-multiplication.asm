a4f3696d65e83bb8a2233ec42b90a02b3efaa2dc
youngbw <Athero23@gmail.com>
2015-01-18 00:08:52 -0800

    Multiplication sequence mostly done, indexing for putting values into new matrix has to be fixed. Cant make call better because of iterative nature of program desgin.

--- matrices-multiplication.asm
+++ matrices-multiplication.asm
@@ -9,6 +9,7 @@ base:		.asciiz "Base Address: "
 sum_address:	.asciiz "Generated Address: "
 m_label_one:	.asciiz "Matrix One"
 m_label_two:	.asciiz "Matrix Two"
+m_label_prod:	.asciiz "Matrix Product"
 space:		.asciiz " "
 new_line: 	.asciiz "\n"
 
@@ -251,14 +252,142 @@ cont_2:	#calculate correct offset using the row major formula
 	
 conduct_operation:
 
-	la $t0, matrix_one
-	la $t1, matrix_two
+	la $s0, matrix_one
+	la $s1, matrix_two
+	la $s2, matrix_product
 
+	li $t1, 16	# number of cols
+	li $t0, 0 	# overall counter
+	li $t2, 0	# local counter
+	
+	move $t3, $zero	# use for matrix 1 value
+	move $t4, $zero # use for matrix 2 value
+	move $t5, $zero # for mult result
+	
+	move $s3, $zero	# use for storing value to move to memory
+	
+	#Free registers = s4, s5, s6, s7, t6, t7
+	
+Beg:
+	addi	$t7, $t0, -16
+	beqz 	$t7, print_matrix_product
+	
+Local_Loop:
+	#find matrix 1 value
+	mult     $t1, $t0       # mult overall count * # cols
+        mflo     $t6            # move result to t6
+        add      $t6, $t6, $t2  # result += local counter
+        sll      $t6, $t6, 2    # shift for offset, save this for offset in product matrix
+        add	 $t7, $t6, $s0
+        lw	 $t3, ($t7)	# save matrix 1 value in t3
+        add	 $s4, $t6, $zero#save offset in s4
+        
+        #find matrix 2 value
+        mult	$t2, $t1	# mult # of cols * local counter
+        mflo	$t6		# move result to t6
+        add	$t6, $t6, $t0	# result += overall counter
+        sll	$t6, $t6, 2
+        add	$t7, $s1, $t6
+        lw	$t4, ($t7)	# save matrix 2 value in t4
+        
+        mult	$t3, $t4	# multiply the two values
+        mflo	$t5		# grab mult result
+        add	$s3, $s3, $t5	# add new result to existing ongoing row/col mult
+        
+	
+	addi	$t2, $t2, 1	# increment local counter
+	addi	$t7, $t2, -16	
+	bnez	$t7, Local_Loop	# for check to see if we are at end of mult sequence for one set of row/col
+	
+	add	$t7, $s2, $s4
+	sw	$s3, ($t7)	# else : we are at end of mult seq, need to store result
+	move	$s3, $zero	# reset running total
+	move	$t2, $zero	# reset local counter
+	addi	$t0, $t0, 1	# increment overall counter (move on to next row/col pair)
+	j	Beg
+	
+	
+print_matrix_product:
 
+    	#################################PRINT_ROUTINE#########################################################
+	li $v0, 4
+	la $a0, new_line
+	syscall
+	
+	li $v0, 4
+	la $a0, new_line
+	syscall
+	
+	li $v0, 4
+	la $a0, m_label_prod
+	syscall
+	
+	li $v0, 4
+	la $a0, new_line
+	syscall
+	#################################PRINT_ROUTINE#########################################################
+	
+	li $t0, 64 #mod operand for determining line breaks (16 units * 4 bytes)
+	
+	la $s2, matrix_product #Grab starting address of matrix two
+	move $s6, $zero #Row (Y) index that will be incremented every time a new line is printed
+	move $s7, $zero #Column (X) index that will incremented every time and reset when a new line is printed.
+	
+	li $t5, 0 #A temporary counter variable
+	
+# Accesses will happen easiest if we utilize row-major order
+# Example: size_of_data_type * (num_total_columns * x_coord + y_coord) = offset_from_base 
 
+itr_product:
+	sub  $t2, $s4, $t5 #need to subtract index instead of counter
+	beqz $t2, end #branch to end ###END IF DEBUGGING
 
-
+	# Else, check the modulus operation 
+	div $t5, $t0 
+	mfhi $t3
+	
+	bnez $t3, cont_product #If remainder is not zero continue past newline jump
+	
+	###################NEW_LINE######################################################										
+	li $v0, 4 #New line printed every 16 entries					#
+	la $a0, new_line 								#
+	li $s7, 0 #Set Column Counter back to 0						#	
+	syscall 									#
+	###################NEW_LINE######################################################	
+cont_product:	#calculate correct offset using the row major formula
+	li $t3, 16 #loads num of columns into temp register
+	mult $t3, $s7
+	mfhi $t3
+	add $t3, $t3, $s6
+	sll $t3, $t3, 2 #Multiply by size of the data type (4 bytes)
+	
+	#add generated offset to the base address
+	la $t4, matrix_product
+	add $t4, $t4, $t3
+	
+	#Load the data located at the memory address stored in $t4 and place it in $t3
+	lw $t3, ($t4)
+	
+	#################################PRINT_ROUTINE#########################################################
+	li $v0, 1 
+	move $a0, $t3 
+	syscall #Print the value at the index to the screen
+	
+	li $v0, 4
+	la $a0, space
+	syscall #Print a space to the screen
+	
+	addi $s7, $s7, 1 #Increment the Column Counter by 1
+	addi $s6, $s6, 1 #Increment the Row counter
+	#################################PRINT_ROUTINE#########################################################
+	addi $t5, $t5, 4
+	j itr_product
+	
+	
 end:
+
+	li       $v0, 10        # system service 10 is exit
+        syscall
 
 
 
